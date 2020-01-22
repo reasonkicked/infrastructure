323faeba0bbebde286fc1f6b81daf73ce2a59085
Tomasz Stanislawczyk (PGS Software) <tstanislawczyk@pgs-soft.com>
2020-01-22 14:12:46 +0100

    5

--- test/services/wordpress-ec2-sauber/main.tf
+++ test/services/wordpress-ec2-sauber/main.tf
@@ -5,6 +5,6 @@ module "wordpress-ec2" {
   source = "../../../modules/services/wordpress-ec2"
  ec2_instance_name = "ts-instance-ec2-02"
  db_remote_state_bucket = "reasonkicked-infrastructure-s4"
-  db_remote_state_key = "/test/services/wordpress-ec2-sauber/terraform.tfstate"
+  db_remote_state_key = "test/services/wordpress-ec2-sauber/terraform.tfstate"
 }
 
