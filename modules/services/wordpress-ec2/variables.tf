1a6e79475060926838c0d4c35d14b6100b2c8c6b
Tomasz Stanislawczyk (PGS Software) <tstanislawczyk@pgs-soft.com>
2020-01-22 15:29:35 +0100

    7

--- modules/services/wordpress-ec2/variables.tf
+++ modules/services/wordpress-ec2/variables.tf
@@ -2,7 +2,8 @@ variable "ec2_instance_name" {
   description = "The name to use for all the cluster resources"
   type        = string
 }
-variable  "ami_key_pair_name"
-{
-  
+
+variable  "ami_key_pair_name" {
+  description = "opis"
+
 }
