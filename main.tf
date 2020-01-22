686572ce3fa8331adfabbe4926a1bb41dc7db4e8
Tomasz Stanislawczyk (PGS Software) <tstanislawczyk@pgs-soft.com>
2020-01-22 14:54:07 +0100

    6

--- main.tf
+++ main.tf
@@ -12,33 +12,5 @@ terraform {
 
 }
 
-resource "aws_instance" "ts-example-00" {
-  ami                    = "ami-02df9ea15c1778c9c"
-  instance_type          = "t2.micro"
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World 202" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
-
-  tags = {
-    Name = "terraform-infrastructure-example"
-    Owner = "tstanislawczyk"
-    Description = "opis testowy"
-  }
-  lifecycle {
-    create_before_destroy = true
-  }
-
-}
-resource "aws_security_group" "instance-jenkins" {
-  name = "terraform-infrastructure-example"
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
--- test/services/wordpress-ec2-sauber/main.tf
+++ test/services/wordpress-ec2-sauber/main.tf
@@ -5,18 +5,9 @@ module "wordpress-ec2" {
   source = "../../../modules/services/wordpress-ec2"
  ec2_instance_name = "ts-instance-ec2-03"
 
-terraform {
-  backend "s3" {
-    # Replace this with your bucket name!
-    bucket         = "reasonkicked-infrastructure-s3"
-    key            = "test/services/wordpress-ec2-sauber/terraform.tfstate"
-    region         = "eu-west-1"
 
-    # Replace this with your DynamoDB table name!
-    dynamodb_table = "terraform-up-and-running-locks"
-    encrypt        = true
-  }
 }
+
 }
 
 
