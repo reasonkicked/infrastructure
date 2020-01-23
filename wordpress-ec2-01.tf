bcacf2a5075a7d130fc799d9e0346055329664e5
Tomasz Stanislawczyk (PGS Software) <tstanislawczyk@pgs-soft.com>
2020-01-23 13:01:48 +0100

    2

--- wordpress-ec2-01.tf
+++ wordpress-ec2-01.tf
@@ -12,7 +12,13 @@ resource "aws_instance" "ts-example-00" {
               apt install php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl -y
               apt install openssh-server
               apt update -y       
-              service mysql start                      
+              service mysql start  
+              mysql -u USER -pPASSWORD <<EOF
+                CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+                GRANT ALL ON wordpress.* TO 'wordpressuser'@'localhost' IDENTIFIED BY 'password';
+                FLUSH PRIVILEGES;
+                EXIT;
+                EOF                    
               cd /var/www/html
               wget http://wordpress.org/latest.tar.gz
               tar -xzf latest.tar.gz
